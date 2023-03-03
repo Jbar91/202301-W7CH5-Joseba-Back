b8bdb237ffd51527bd9741de981b4274ad630fb5
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-03-04 00:10:14 +0100

    Add users repo interface and mongo.model

---
+++ src/repo/users/users.mongo.models.ts
@@ -0,0 +1,39 @@
+import { model, Schema } from 'mongoose';
+import { User } from '../../entities/user';
+
+const userSchema = new Schema<User>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwd: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  friends: {
+    type: [],
+  },
+  enemies: {
+    type: [],
+  },
+});
+
+userSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject._id;
+    delete returnedObject.passwd;
+  },
+});
+
+export const UserModel = model('User', userSchema, 'users');
