2b6f166b1d4533478c17f70bd7aabde53cd2a6de
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-03-04 11:24:08 +0100

    Add errors middleware and users router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { CustomError } from './interfaces/errors.js';
+import createDebug from 'debug';
+import { usersRouter } from './routers/users.router.js';
+
+const debug = createDebug('FRNMS: app');
 
 export const app = express();
 
@@ -12,3 +17,23 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors(corsOptions));
 app.disable('x-powered-by');
+
+app.use('/users', usersRouter);
+
+app.use(
+  (error: CustomError, _req: Request, resp: Response, _next: NextFunction) => {
+    debug('Soy un middleware de errores');
+    const status = error.statusCode || 500;
+    const statusMsg = error.statusMsg || 'Internal server error';
+    resp.status(status);
+    resp.json({
+      error: [
+        {
+          status,
+          statusMsg,
+        },
+      ],
+    });
+    debug(status, statusMsg, error.message);
+  }
+);
